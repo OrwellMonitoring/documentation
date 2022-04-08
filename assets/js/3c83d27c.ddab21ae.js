6f346ef311b60f4191382ba38060fc109cb391c6
goncalo-leal <goncalolealsilva@ua.pt>
2022-04-09 00:29:13 +0100

    baseUrl changed

---
+++ assets/js/3c83d27c.ddab21ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkorwell_docs=self.webpackChunkorwell_docs||[]).push([[431],{8270:function(o){o.exports=JSON.parse('{"permalink":"/documentation/blog/tags/hello","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
